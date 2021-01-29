34c11e6d324441e84f0d9a0162ed96795ddff86b
Yusuf <myusuffathussalam@gmail.com>
2021-01-29 08:43:38 +0700

    commit erase stylesheet

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer, TabActions } from '@react-navigation/native';
 
-import { StyleSheet, Text, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons'; 
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
@@ -44,9 +43,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-});
